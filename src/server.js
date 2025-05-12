b7d2728dbb8b20e2a389d9dc743c2eb1e6c11785
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-05-12 14:49:40 +0700

    create env file for credentials

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
-    port: '5000',
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
