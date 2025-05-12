2478dc178921b1b3fd903a80717499267a447d72
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-05-12 14:35:51 +0700

    setup project and create routes

---
+++ src/api/songs/songsRoutes.js
@@ -0,0 +1,29 @@
+const songsRoutes = (handler) => [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: handler.postSongsHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: handler.getSongsHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs/{id}',
+    handler: handler.getSongsByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{id}',
+    handler: handler.putSongsByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{id}',
+    handler: handler.deleteSongsByIdHandler,
+  },
+];
+
+module.exports = songsRoutes;
