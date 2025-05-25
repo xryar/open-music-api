9364f5ddfc21c3080f2548b15a471195682ec086
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-05-25 11:52:23 +0700

    create auth plugin

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,67 @@
+const autoBind = require('auto-bind');
+
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAuthenticationHandler(request, h) {
+    this._validator.validatePostAuthenticationPayload(request.payload);
+
+    const { username, password } = request.payload;
+    const id = await this._usersService.verifyUserCredential(username, password);
+
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+    const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+    await this._authenticationsService.addRefreshToken(refreshToken);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Authentication berhasil ditambahkan',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async putAuthenticationHandler(request, h) {
+    this._validator.validatePutAuthenticationPayload(request.payload);
+
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefrehToken(refreshToken);
+    const { id } = this._tokenManager.verifyRefrehToken(refreshToken);
+
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+    return h.response({
+      status: 'success',
+      message: 'Access Token berhasil diperbarui',
+      data: {
+        accessToken,
+      },
+    });
+  }
+
+  async deleteAuthenticationHandler(request, h) {
+    this._validator.validateDeleteAuthenticationPayload(request.payload);
+
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefrehToken(refreshToken);
+    await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+    return h.response({
+      status: 'success',
+      message: 'Refresh token berhasil dihapus',
+    });
+  }
+}
+
+module.exports = AuthenticationsHandler;
