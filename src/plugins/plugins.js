71498f57b60d3ea2cb5a539c5208e67e15f48101
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-05-25 11:58:15 +0700

    auth plugin implemented

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -1,33 +1,51 @@
 const AlbumsService = require('../services/postgres/AlbumsService');
 const SongsService = require('../services/postgres/SongsService');
 const UsersService = require('../services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 const albums = require('../api/albums');
 const songs = require('../api/songs');
+const users = require('../api/users');
+const authentications = require('./api/authentications');
 const AlbumsValidator = require('../validator/albums');
 const SongsValidator = require('../validator/songs');
 const UsersValidator = require('../validator/users');
-const users = require('../api/users');
+const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
+
+const usersService = new UsersService();
+const albumsService = new AlbumsService();
+const songsService = new SongsService();
+const authenticationsService = new AuthenticationsService();
 
 module.exports = [
   {
     plugin: albums,
     options: {
-      service: new AlbumsService,
+      service: albumsService,
       validator: AlbumsValidator
     },
   },
   {
     plugin: songs,
     options: {
-      service: new SongsService,
+      service: songsService,
       validator: SongsValidator
     },
   },
   {
     plugin: users,
     options: {
-      service: new UsersService,
+      service: usersService,
       validator: UsersValidator,
+    }
+  },
+  {
+    plugin: authentications,
+    options: {
+      authenticationsService,
+      usersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator,
     }
   }
 ];
