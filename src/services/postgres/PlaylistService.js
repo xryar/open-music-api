8cdeae60fea853d38e6b3fa0682abe147ec649ad
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-05-28 08:50:52 +0700

    make query uppercase

--- src/services/postgres/PlaylistService.js
+++ src/services/postgres/PlaylistService.js
@@ -83,7 +83,7 @@ class PlaylistService {
     const songsQuery = {
       text: `SELECT songs.id, songs.title, songs.performer
       FROM playlist_songs
-      JOIN songs on playlist_songs.songs_id = songs.id
+      JOIN songs ON playlist_songs.songs_id = songs.id
       WHERE playlist_songs.playlist_id = $1`,
       values: [playlistId],
     };
